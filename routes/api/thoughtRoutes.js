2cb0d56cf34452b50908bfd266b1996b10f11e61
Giancarlo Morera <giancarlomorera@giancarlos-air.lan>
2024-04-02 23:10:43 -0400

    added API routes

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,13 @@
+const router = require('express').Router();
+const {
+  getThoughts,
+  getSingleThought,
+  updateThought,
+  createThought,
+  createReaction,
+  deleteReaction,
+} = require('../../controllers/thoughtController.js');
+router.route('/').get(getThoughts).post(createThought)
+router.route('/:thoughtId').get(getSingleThought).put(updateThought).delete(deleteThought);
+router.route('/:thoughtId/reactions').post(createReaction).delete(deleteReaction)
+module.exports = router;
