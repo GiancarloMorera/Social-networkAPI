d4f8e5de0a241d40cff4caa362a481d33f669fa1
Giancarlo Morera <giancarlomorera@giancarlos-air.lan>
2024-04-03 20:56:29 -0400

    updated ReadMe

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Social-networkAPI
+
+REPO URL: https://github.com/GiancarloMorera/Social-networkAPI 
+
+Sources: Troy Mena, Tutor Jose Lopez, Danny Saravia, 
--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -6,6 +6,7 @@ const {
   createThought,
   createReaction,
   deleteReaction,
+  deleteThought,
 } = require('../../controllers/thoughtController.js');
 router.route('/').get(getThoughts).post(createThought)
 router.route('/:thoughtId').get(getSingleThought).put(updateThought).delete(deleteThought);
